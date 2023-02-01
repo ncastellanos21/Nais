e1d43387c8707c918ee4c4baa9eee8d942a8fb0b
ncastellanos21 <92100837+ncastellanos21@users.noreply.github.com>
2023-02-01 19:56:01 +0100

    Delete page directory

--- page/js/form.js
+++
@@ -1,9 +0,0 @@
-const fechaNacimiento = document.getElementById("fechaNacimiento");
-const edad = document.getElementById("edad");
-
-window.addEventListener('load', function () {
-
-    fechaNacimiento.addEventListener('change', function () {
-        console.log(this.value);
-    });
-});
